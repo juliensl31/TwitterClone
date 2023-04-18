eb590935d1f81e480e5f3b4b5bf95d1b4bbfd46a
julien lizano <julienlizano@gmail.com>
2023-04-18 17:47:06 +0200

    finalize share option

--- src/Components/DisplayedTweets/DisplayedTweet/DisplayedTweet.js
+++ src/Components/DisplayedTweets/DisplayedTweet/DisplayedTweet.js
@@ -4,6 +4,12 @@ import { Link } from 'react-router-dom';
 import routes from '../../../config/routes';
 import classes from './DisplayedTweet.module.css';
 import PropTypes from 'prop-types';
+import {ShareSocial} from 'react-share-social';
+import moment from 'moment';
+import 'moment/locale/fr';
+
+
+import ShareModal from '../../UI/ShareModal/ShareModal';
 
 function DisplayedTweet(props) {
 
@@ -11,31 +17,42 @@ function DisplayedTweet(props) {
     // Raccourcir affichage post
     const substr = props.tweet.contenu.substr(1, 150);
 
+    // Variable
+    moment.locale('fr');
+    let date = moment.unix(props.tweet.date / 1000).startOf('hour').fromNow();
+
     return (
-        <Link className={classes.link} to={routes.TWEETS + '/' + props.tweet.slug}>
-            <div className={classes.DisplayedTweet}>
+        <div className={classes.DisplayedTweet}>
+            <Link className={classes.link} to={routes.TWEETS + '/' + props.tweet.slug}>
                 <h2>{props.tweet.titre}</h2>
                 <p>{substr + " ..."}</p>
-                <div className={classes.footer}>
-                    <small>{props.tweet.auteur}</small>
-                    <div className={classes.icons}>
-                        <small className={classes.hashtag}>{props.tweet.hashtag}</small>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-                        </svg>
-                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
-                        </svg> 
-                    </div>
-                   
+                <small className={classes.hashtag}>{props.tweet.hashtag}</small>
+            </Link>    
+            <div className={classes.footer}>
+                <div className={classes.footerLeft}>
+                    <b>{props.tweet.auteur}</b>
+                    <small>{date}</small>  
+                </div>
+                <div className={classes.icons}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                        <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                    </svg>
+                    <ShareModal>
+                        <ShareSocial
+                            className={classes.ShareSocial}
+                            url={"http://localhost:3000/TwitterClone/tweets/" + props.tweet.slug}
+                            socialTypes={['facebook', 'twitter', 'whatsapp', 'reddit', 'linkedin', 'telegram', 'email']}
+                            onSocialButtonClicked={data => console.log(data)}
+                        />
+                    </ShareModal>
                 </div>
             </div>
-        </Link>
+        </div>
+
     );
 }
 DisplayedTweet.propTypes = {
--- src/Containers/Tweets/Tweet/Tweet.js
+++ src/Containers/Tweets/Tweet/Tweet.js
@@ -7,8 +7,10 @@ import routes from '../../../config/routes';
 import classes from './Tweet.module.css';
 import moment from 'moment';
 import 'moment/locale/fr';
+import {ShareSocial} from 'react-share-social';
 
 import Post from '../../../Components/UI/Comment/Post';
+import ShareModal from '../../../Components/UI/ShareModal/ShareModal';
 
 function Tweet(props) {
 
@@ -92,10 +94,14 @@ function Tweet(props) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                         </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
-                        </svg>  
+                        <ShareModal>
+                            <ShareSocial 
+                                className={classes.ShareSocial} 
+                                url = {"http://localhost:3000/TwitterClone/" + props.match.url}
+                                socialTypes= {['facebook','twitter', 'whatsapp', 'reddit', 'linkedin', 'telegram', 'email']}
+                                onSocialButtonClicked={ data => console.log(data)}    
+                            />
+                        </ShareModal>
                         {props.user.displayName === tweet.auteur ? 
                         <>
                             <svg onClick={deleteClickedHandler} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
