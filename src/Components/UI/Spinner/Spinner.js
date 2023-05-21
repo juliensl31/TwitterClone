b5d3d66bf20695c2b836d49d055ff3959546ec50
juliensl31 <47530405+juliensl31@users.noreply.github.com>
2023-05-21 20:39:05 +0200

    Updates

--- src/Components/UI/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-// Librairies
-import React from 'react';
-import classes from './Spinner.module.css';
-
-// Composant Spinner
-const Spinner = () => <div className={classes.loader}>Chargement...</div>;
-
-export default Spinner;
